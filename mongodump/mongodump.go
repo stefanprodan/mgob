a4b27af27ab28a0d1ef2994c052e1843e3d944fa
Stefan Prodan <stefan.prodan@gmail.com>
2017-05-02 12:59:00 +0300

    apply retention

--- mongodump/mongodump.go
+++ mongodump/mongodump.go
@@ -13,23 +13,31 @@ func Run(plan config.Plan, conf *config.AppConfig) error {
 
 	err := sh.Command("mkdir", "-p", fmt.Sprintf("%v/%v", conf.StoragePath, plan.Name)).Run()
 	if err != nil {
-		return errors.Wrapf(err, "creating dir in %v failed", conf.StoragePath)
+		return errors.Wrapf(err, "creating dir %v in %v failed", plan.Name, conf.StoragePath)
 	}
 
 	ts := time.Now().UTC().Unix()
 	archive := fmt.Sprintf("%v/%v/%v.gz", conf.StoragePath, plan.Name, ts)
 	log := fmt.Sprintf("%v/%v/%v.log", conf.StoragePath, plan.Name, ts)
 
-	cmd := fmt.Sprintf("mongodump --archive=%v --gzip --host %v --port %v --db %v ",
+	dump := fmt.Sprintf("mongodump --archive=%v --gzip --host %v --port %v --db %v ",
 		archive, plan.Target.Host, plan.Target.Port, plan.Target.Database)
 	if plan.Target.Username != "" && plan.Target.Password != "" {
-		cmd += fmt.Sprintf("-u %v -p %v", plan.Target.Username, plan.Target.Password)
+		dump += fmt.Sprintf("-u %v -p %v", plan.Target.Username, plan.Target.Password)
 	}
 
-	output, err := sh.Command("/bin/sh", "-c", cmd).SetTimeout(time.Duration(plan.Scheduler.Timeout) * time.Minute).CombinedOutput()
+	output, err := sh.Command("/bin/sh", "-c", dump).SetTimeout(time.Duration(plan.Scheduler.Timeout) * time.Minute).CombinedOutput()
 	logToFile(log, output)
 	if err != nil {
 		return errors.Wrapf(err, "mongodump failed, see log %v", log)
+	}
+
+	if plan.Scheduler.Retention > 0 {
+		rootDir := fmt.Sprintf("%v/%v", conf.StoragePath, plan.Name)
+		err = applyRetention(rootDir, plan.Scheduler.Retention)
+		if err != nil {
+			return errors.Wrap(err, "retention job failed")
+		}
 	}
 
 	return nil
@@ -41,6 +49,24 @@ func logToFile(file string, data []byte) error {
 		if err != nil {
 			return errors.Wrapf(err, "writing log %v failed", file)
 		}
+	}
+
+	return nil
+}
+
+func applyRetention(path string, retention int) error {
+	gz := fmt.Sprintf("cd %v && rm -f $(ls -1t *.gz | tail -n +%v)", path, retention+1)
+
+	err := sh.Command("/bin/sh", "-c", gz).Run()
+	if err != nil {
+		return errors.Wrapf(err, "removing old gz files from %v failed", path)
+	}
+
+	log := fmt.Sprintf("cd %v && rm -f $(ls -1t *.log | tail -n +%v)", path, retention+1)
+
+	err = sh.Command("/bin/sh", "-c", log).Run()
+	if err != nil {
+		return errors.Wrapf(err, "removing old log files from %v failed", path)
 	}
 
 	return nil
