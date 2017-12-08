bfa104d6fa96a9146a2f6be263b3c5ae2f63ccb3
Mathew Peterson <mathewpeterson@me.com>
2017-12-08 10:56:25 -0600

    Adds CheckGCloudClient

--- backup/checks.go
+++ backup/checks.go
@@ -32,3 +32,16 @@ func CheckMinioClient() (string, error) {
 
 	return strings.Replace(string(output), "\n", " ", -1), nil
 }
+
+func CheckGCloudClient() (string, error) {
+	output, err := sh.Command("/bin/sh", "-c", "gcloud --version").CombinedOutput()
+	if err != nil {
+		ex := ""
+		if len(output) > 0 {
+			ex = strings.Replace(string(output), "\n", " ", -1)
+		}
+		return "", errors.Wrapf(err, "gcloud failed %v", ex)
+	}
+
+	return strings.Replace(string(output), "\n", " ", -1), nil
+}
