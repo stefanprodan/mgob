c29f130048585c1f687ed8a67dce93f5ab8ddf9f
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-03-20 15:48:40 -0300

    add firt test

--- backup/restore/restore_test.go
+++ backup/restore/restore_test.go
@@ -1,8 +1,27 @@
 package restore_test
 
 import (
+	"github.com/stefanprodan/mgob/backup/restore"
+	"github.com/stefanprodan/mgob/config"
 	"testing"
 )
 
-func TestChangingGetReturnConfigShouldNotChangeInternalConfig(t *testing.T) {
+func assertError(t *testing.T, err error) {
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMungoRestoreReturnErrorOnInvalidArchive(t *testing.T) {
+	target := config.Target{
+		Host: "localost",
+		Port: 27017,
+	}
+	sched := config.Scheduler{Timeout: 60}
+	plan := config.Plan{
+		Target:    target,
+		Scheduler: sched,
+	}
+	err := restore.Restore(plan, "invalid")
+	assertError(t, err)
 }
