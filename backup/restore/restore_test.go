fd3cbef3451469cb112fdd5e3331589f069857ff
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-03-21 17:47:02 -0300

    test start and shutdown mongod

--- backup/restore/restore_test.go
+++ backup/restore/restore_test.go
@@ -1,20 +1,29 @@
 package restore_test
 
 import (
+	"testing"
+
 	"github.com/stefanprodan/mgob/backup/restore"
 	"github.com/stefanprodan/mgob/config"
-	"testing"
 )
 
 func assertError(t *testing.T, err error) {
+	t.Log(err)
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Log(err)
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestMungoRestoreReturnErrorOnInvalidArchive(t *testing.T) {
+func TestMongoRestoreReturnErrorOnInvalidHost(t *testing.T) {
 	target := config.Target{
-		Host: "localost",
+		Host: "invalid",
 		Port: 27017,
 	}
 	sched := config.Scheduler{Timeout: 60}
@@ -24,4 +33,37 @@ func TestMungoRestoreReturnErrorOnInvalidArchive(t *testing.T) {
 	}
 	err := restore.Restore(plan, "invalid")
 	assertError(t, err)
+}
+
+func TestMongoRestoreReturnErrorOnInvalidArchive(t *testing.T) {
+	target := config.Target{
+		Host: "localhost",
+		Port: 27017,
+	}
+	sched := config.Scheduler{Timeout: 60}
+	plan := config.Plan{
+		Target:    target,
+		Scheduler: sched,
+	}
+	err := restore.Restore(plan, "invalid")
+	assertError(t, err)
+}
+
+func setUp(localhost string, port int) config.Plan {
+	target := config.Target{
+		Host: "localhost",
+		Port: 27017,
+	}
+	sched := config.Scheduler{Timeout: 60}
+	plan := config.Plan{
+		Target:    target,
+		Scheduler: sched,
+	}
+	return plan
+}
+
+func TestMongoRestoreWithSucess(t *testing.T) {
+	plan := setUp("localhost", 27017)
+	err := restore.Restore(plan, "/tmp/dump_test.gz")
+	assertNoError(t, err)
 }
