e39396330f6dcf069212ea66c20fa9dce50e986f
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-03-22 14:41:27 -0300

    start check backup

--- backup/restore/restore.go
+++ backup/restore/restore.go
@@ -9,6 +9,13 @@ import (
 	"github.com/codeskyblue/go-sh"
 	"github.com/pkg/errors"
 	"github.com/stefanprodan/mgob/config"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	host = "127.0.0.1"
+	port = 27017
 )
 
 // Responsible to restore backup from one source
@@ -24,7 +31,7 @@ func Restore(plan config.Plan, archive string) error {
 	}
 	defer shutdownMongo()
 	restore := fmt.Sprintf("mongorestore --archive=%v --gzip --host %v --port %v ",
-		archive, plan.Target.Host, plan.Target.Port)
+		archive, host, port)
 	if plan.Target.Database != "" {
 		restore += fmt.Sprintf("--db %v ", plan.Target.Database)
 	}
@@ -37,6 +44,30 @@ func Restore(plan config.Plan, archive string) error {
 		}
 		return errors.Wrapf(err, "mongorestore log %v", ex)
 	}
+	fmt.Printf("%s\n", output)
+	err = checkRestore(host, port)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkRestore(host string, port int) error {
+	mongoURL := fmt.Sprintf("mongodb://%v:%d", host, port)
+	session, err := mgo.Dial(mongoURL)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	session.SetSafe(&mgo.Safe{})
+	c := session.DB("garden").C("parameters")
+
+	count, err2 := c.Find(bson.M{}).Count()
+	if err2 != nil {
+		return err
+	}
+	fmt.Printf("total  parameters count = %d\n", count)
 	return nil
 }
 
