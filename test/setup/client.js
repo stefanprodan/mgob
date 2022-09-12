d5b61cfd00dcedbd8374ae020699880d69482b03
Sam Lin <maxisam@gmail.com>
2022-09-11 23:43:31 -0500

    feat: add gh action (#4)
    add dev & release build

---
+++ test/setup/client.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const PORT = process.env.MONGODB_PORT || 27017;
+const HOST = process.env.MONGODB_HOST || "localhost";
+const DBNAME = process.env.MONGODB_DB_NAME || "test";
+
+async function main() {
+  const client = new MongoClient(`mongodb://${HOST}:${PORT}/${DBNAME}`, {
+    useUnifiedTopology: true,
+  });
+  try {
+    await client.connect();
+    await client.db().collection("test").insertOne({
+      item: "item",
+      val: "test",
+    });
+    const result = await client.db().collection("test").findOne({});
+    console.log(JSON.stringify(result));
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
